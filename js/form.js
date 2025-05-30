c7581dc6e3b55dca487499f5d76437a5806cfc06
vladislavshp <vladislavshp@gmail.com>
2025-05-30 20:56:21 +0800

    Валидация формы хэш тэгов

--- js/form.js
+++ js/form.js
@@ -4,6 +4,8 @@ const imgUpload = document.querySelector('.img-upload__input');
 const imgContainer = document.querySelector('.img-upload__overlay');
 const closeBtn = document.querySelector('.img-upload__cancel');
 const form = document.querySelector('.img-upload__form');
+const formDescription = document.querySelector('.text__description');
+const hashTag = document.querySelector('.text__hashtags');
 
 const onCloseBtnClick = () => {
   hideModal();
@@ -12,6 +14,17 @@ const onCloseBtnClick = () => {
 const onFormSubmit = () => {
   activateValidation();
 };
+
+const isFocused = () => document.activeElement === formDescription || document.activeElement === hashTag;
+
+document.addEventListener('keydown', (evt) => {
+  if (isFocused()) {
+    return;
+  }
+  if (evt.key === 'Escape') {
+    hideModal();
+  }
+});
 
 function hideModal() {
   imgContainer.classList.add('hidden');
--- js/validation.js
+++ js/validation.js
@@ -2,10 +2,11 @@ import { checkMaxLengthString } from './utils.js';
 
 const form = document.querySelector('.img-upload__form');
 const formDescription = document.querySelector('.text__description');
+const hashTag = document.querySelector('.text__hashtags');
 
-const validateText = (str) => {
-  checkMaxLengthString(str, 140);
-};
+const re = /^#[A-Za-zА-Яа-яЁё0-9]+$/;
+
+const validateText = (str) => checkMaxLengthString(str, 140);
 
 const pristine = new Pristine(form, {
   classTo: 'img-upload__wrapper',
@@ -22,5 +23,24 @@ const deactivateValidation = () => {
 };
 
 pristine.addValidator(formDescription, validateText, 'Длина строки не может быть больше 140 символов');
+
+const getHashTags = (str) => str.toLowerCase().split(' ');
+
+const checkHashTagsSymbols = (value) => !hashTag || getHashTags(value).every((el) => re.test(el));
+pristine.addValidator(hashTag, checkHashTagsSymbols, 'Хэш-тег начинается с символа # (решётка)');
+
+const checkLength = (value) => getHashTags(value).every((el) => el.length <= 20);
+pristine.addValidator(hashTag, checkLength, 'Длина хэш-тега не должна превышать 20 символов');
+
+const checkMaxHashTags = (value) => getHashTags(value).length <= 5;
+pristine.addValidator(hashTag, checkMaxHashTags, 'Количество хэш-тегов не может быть больше 5');
+
+const checkUniqueHashTag = (value) => {
+  const hashTags = getHashTags(value);
+  const uniqueHashTags = new Set(hashTags);
+
+  return hashTags.length === uniqueHashTags.size;
+};
+pristine.addValidator(hashTag, checkUniqueHashTag, 'Хэш-теги должны быть уникальны');
 
 export { activateValidation, deactivateValidation };
